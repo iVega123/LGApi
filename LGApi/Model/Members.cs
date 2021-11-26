bd7ce2efe0884fd966193a0291d3524f767e2524
caio.viga <caio.viga@dmz.org.br>
2021-11-26 19:40:28 -0400

    Finished LGApi

---
+++ LGApi/Model/Members.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MongoDB.GenericRepository.Model
+{
+    public class Members
+    {
+        public Members(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+
+        public Members(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Name { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # LGApi
+
+# ASP.NET WEB API
+
+
+* **Server Side**: ASP.NET Core
+* **Persistencia**: MongoDb  
+
+Pre requisitos
+* .Net 5.0
+
+  
+
+Techs:
+
+* ASP.NET Core
+
+* Swagger
+
+* ServiceStack.Core
