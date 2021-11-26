db3906535c4ea419f8b66723370d677f5656efe8
caio.viga <caio.viga@dmz.org.br>
2021-11-26 19:24:07 -0400

    Persistence Finished

---
+++ LGApi/Persistence/MongoDbPersistence.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace MongoDB.GenericRepository.Persistence
+{
+    public static class MongoDbPersistence
+    {
+        public static void Configure()
+        {
+            ProjectMap.Configure();
+
+            BsonDefaults.GuidRepresentation = GuidRepresentation.CSharpLegacy;
+
+            // Conventions
+            var pack = new ConventionPack
+                {
+                    new IgnoreExtraElementsConvention(true),
+                    new IgnoreIfDefaultConvention(true)
+                };
+            ConventionRegistry.Register("My Solution Conventions", pack, t => true);
+        }
+    }
+}
